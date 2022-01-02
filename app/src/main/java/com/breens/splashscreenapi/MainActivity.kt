94499da8c4c9e5d8f13ca575ea6b08486f606cb1
user <user@users-MacBook-Air.local>
2022-01-02 15:02:37 +0300

    Implementing Android Splash Api

---
+++ app/src/main/java/com/breens/splashscreenapi/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.breens.splashscreenapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepVisibleCondition{
+                viewModel.isLoading.value
+            }
+        }
+        setContentView(R.layout.activity_main)
+    }
+}
